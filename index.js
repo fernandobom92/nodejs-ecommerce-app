fefd8c0c7f848a3cd14c81fdf47b15f491f1f4f6
fernandobom92 <fernandobom92@gmail.com>
2022-06-22 10:30:08 -0300

    Segundo commits

--- index.js
+++ index.js
@@ -4,17 +4,23 @@ const app = express();
 
 //req abaixo faz as requisições para o webserver
 //res abaixo retorna a resposta do webserver
+//get recebe apenas requisições do tipo GET
 app.get('/', (req, res) =>{
     res.send(`
         <div>
-            <form>
-                <input placeholder="email" />
-                <input placeholder="password" />
-                <input placeholder="password confirmation" />
+            <form method="POST">
+                <input name="email" placeholder="email" />
+                <input name="password" placeholder="password" />
+                <input name ="passwordConfirmation" placeholder="password confirmation" />
                 <button>Signup</button>      
             </form>
         </div>    
     `); //manda uma resposta do server para quem faz a requisição
+});
+
+//post recebe apenas requisições do tipo POST
+app.post('/', (req,res) =>{
+    res.send('<h1>Conta criada com sucesso!</h1>');
 });
 
 //começa a ouvir a porta 3000, envia uma HTTP request
