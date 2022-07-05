ce52af4bc59affda8e326510c0a7b5ef9f335aa0
fernandobom92 <fernandobom92@gmail.com>
2022-07-05 10:36:36 -0300

    pagina de produtos melhorada

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { check, validationResult } = require('express-validator'); //colchetes serve para pegar apenas a função especifica do pacote
-const usersRepo = require('../../repositories/users.js');
-const signupTemplate = require('../../views/admin/auth/signup.js');
-const signinTemplate = require('../../views/admin/auth/signin.js');
+
+const { handleErrors } = require('./middlewares');
+const usersRepo = require('../../repositories/users');
+const signupTemplate = require('../../views/admin/auth/signup');
+const signinTemplate = require('../../views/admin/auth/signin');
 const { 
     requireEmail, 
     requirePassword, 
@@ -16,47 +17,41 @@ router.get('/signup', (req, res) => {
     res.send(signupTemplate({ req }));
 });
 
-router.post('/signup', [ requireEmail, requirePassword, requirePasswordConfirmation ],     
+router.post(
+    '/signup',
+    [ requireEmail, requirePassword, requirePasswordConfirmation ],
+    handleErrors(signupTemplate)  ,   
     async (req,res) => {
-        const errors = validationResult(req);
-        
-        if (!errors.isEmpty()) {
-            return res.send(signupTemplate({ req, errors })); //exporta erros somente se houver algum erro de validação
-        }
-
-        const { email, password, passwordConfirmation} = req.body; 
+        const { email, password } = req.body; 
         const user = await usersRepo.create({ email, password });
 
         req.session.userId = user.id;
 
-        res.send('<h1>Conta criada!</h1>');
+        res.redirect('/admin/products');
     }
 );
 
 router.get('/signout', (req, res) => {
     req.session = null;
-    res.send('<h1>Você saiu do sistema!</h1>');
+    res.redirect('/signin');
 });
 
 router.get('/signin', (req,res) => {
     res.send(signinTemplate({})); //objeto vazio evita erros ao receber argumentos vazios
 });
 
-router.post('/signin', [ requireEmailExists, requireValidPasswordForUser ], 
+router.post(
+    '/signin',
+    [ requireEmailExists, requireValidPasswordForUser ],
+    handleErrors(signinTemplate),
     async (req, res) => {
-        const errors = validationResult(req);
-
-        if (!errors.isEmpty()) {
-            return res.send(signinTemplate({ errors }));
-        }
-
         const { email } = req.body;
 
         const user = await usersRepo.getOneBy({ email });
 
         req.session.userId = user.id;
 
-        res.send('Logado corretamente!');
+        res.redirect('/admin/products');
 });
 
 module.exports = router;
--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,29 +1,45 @@
 const express = require('express');
-const { validationResult } = require('express-validator');
 const multer = require('multer');
 
+const { handleErrors, requireAuth } = require('./middlewares');
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
+const productsIndexTemplate = require('../../views/admin/products/index');
 const { requireTitle, requirePrice } = require('./validation');
 
 const router = express.Router();
-
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.get('/admin/products', (req, res) => {
-    res.send('Página de produtos!');
-});
+router.get(
+    '/admin/products', 
+    requireAuth, 
+    async (req, res) => {
+        const products = await productsRepo.getAll();
+        res.send(productsIndexTemplate({ products }));
+    }
+);
 
-router.get('/admin/products/new', (req, res) => {
-    res.send(productsNewTemplate({}));
-});
+router.get(
+    '/admin/products/new',
+    requireAuth,
+    (req, res) => {
+        res.send(productsNewTemplate({}));
+    }
+);
 
-router.post('/admin/products/new', [requireTitle, requirePrice], upload.single('image'), (req, res) => {
-    const errors = validationResult(req);
+router.post(
+    '/admin/products/new',
+    requireAuth,
+    upload.single('image'),
+    [requireTitle, requirePrice], 
+    handleErrors(productsNewTemplate),
+    async (req, res) => {
+        const image = req.file ? req.file.buffer.toString('base64') : '';
+        const { title, price } = req.body;
+        await productsRepo.create({ title, price, image });
 
-    console.log(req.file);
-
-    res.send('submitted');
-});
+        res.redirect('/admin/products');
+    }
+);
 
 module.exports = router;
